aceffaca4ad8cf6557925ba8b91bd252329309a9
aa1446 <205840725+aa1446@users.noreply.github.com>
2025-04-02 15:30:06 +0300

    add check to dw_mstart

--- mach/stat.c
+++ mach/stat.c
@@ -9,12 +9,18 @@ int dw_mstart(const dw_size_t ram_size, struct dw_mstat *mstat)
     mstat->ram.size = ram_size;
     mstat->ram.data = (dw_mdat_t*) calloc(mstat->ram.size, sizeof(dw_mdat_t));
 
+    if (mstat->ram.data == NULL) return 0;
+
     return 1;
 }
 
 int dw_mstop(struct dw_mstat *mstat)
 {
-    free(mstat->ram.data);
+    if (mstat->ram.data != NULL)
+    {
+        free(mstat->ram.data);
 
-    return 1;
+        return 1;
+    }
+    else return 0;
 }
