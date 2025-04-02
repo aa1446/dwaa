5a5a7119ee997ef48fff64af5446f78895b13320
aa1446 <205840725+aa1446@users.noreply.github.com>
2025-04-02 14:35:51 +0300

    add dw_mstat.ram, type definitions in mach/int.h

---
+++ mach/int.h
@@ -0,0 +1,13 @@
+#ifndef DWAA_MACH_INT_H
+#  define DWAA_MACH_INT_H
+
+#  include <stddef.h>
+#  include <stdint.h>
+
+typedef uint8_t dw_8bitd_t;
+typedef uint64_t dw_64bitd_t;
+
+typedef dw_8bitd_t dw_mval_t;
+typedef dw_64bitd_t dw_size_t;
+
+#endif
--- mach/stat.h
+++ mach/stat.h
@@ -1,6 +1,15 @@
 #ifndef DWAA_MACH_STAT_H
 #  define DWAA_MACH_STAT_H
 
-struct dw_mstat {};
+#  include "int.h"
+
+struct dw_mstat
+{
+    struct
+    {
+        dw_size_t size;
+        dw_mval_t *data;
+    } ram;
+};
 
 #endif
