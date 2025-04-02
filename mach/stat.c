8e4a784fadcd3d9e36f76ed625ec1d73c59a9534
aa1446 <205840725+aa1446@users.noreply.github.com>
2025-04-02 15:08:08 +0300

    change pointer styling

--- mach/stat.c
+++ mach/stat.c
@@ -7,7 +7,7 @@
 int dw_mstart(const dw_size_t ram_size, struct dw_mstat *mstat)
 {
     mstat->ram.size = ram_size;
-    mstat->ram.data = (dw_mdat_t *) calloc(mstat->ram.size, sizeof(dw_mdat_t));
+    mstat->ram.data = (dw_mdat_t*) calloc(mstat->ram.size, sizeof(dw_mdat_t));
 
     return 1;
 }
